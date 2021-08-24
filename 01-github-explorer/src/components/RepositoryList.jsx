091a62fd063f3197c4f89e6cbd477ea176cd77c6
John <jhonatha.b13@gmail.com>
2021-08-24 14:59:16 -0400

    add retorno dinamico

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -2,30 +2,24 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-const repository = {
-	name: 'unform2',
-	description: 'form in react',
-	link: 'https://github.com/unform/unform'
-}
-
 
 export function RepositoryList() {
 	const [repositories, setRepositories] = useState([]);
   useEffect(() => {
-    fetch('https://api.github.com/users/jhonathadev/repos')
-      .then(reponse => response.json)
-      .then(data => console.log(data))
-  }, [repositories]);
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
 
 	return(
 		<section className="repository-list">
 			<h1>Lista de repositório</h1>
 
 			<ul>
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
+
+				{repositories.map(repository => {
+					return <RepositoryItem key={repository.name} repository={repository} />
+				})}
 			</ul>
 						
 		</section>
