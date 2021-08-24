43a4fad4ffa71b991604de3a8ae13fae51ea79e7
John <jhonatha.b13@gmail.com>
2021-08-23 20:44:21 -0400

    first chapter ignite

---
+++ 01-github-explorer/src/app.jsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './components/repositorylist';
+import './styles/global.scss';
+
+export function App() {
+    return(
+        
+        <>  {/* <>, </> is called fragment*/}
+            <RepositoryList /> 
+        </>
+    ) 
+}
